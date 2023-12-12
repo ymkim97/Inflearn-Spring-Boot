4ab4736d14b0ea2dde3fe9ac83df2929901ce6f9
ymkim97 <rladudaud97@naver.com>
2023-12-12 19:14:12 +0900

    Servlet Container init study

---
+++ server/src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,29 @@
+package hello.container;
+
+import java.util.Set;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
+
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+		System.out.println("MyContainerInitV2.onStartup");
+		System.out.println("MyContainerInitV2 c = " + c);
+		System.out.println("MyContainerInitV2 ctx = " + ctx);
+
+		//class hello.container.AppInitV1Servlet
+		for (Class<?> appInitClass : c) {
+			try {
+				AppInit appInit = (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+				appInit.onStartup(ctx);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
